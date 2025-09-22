0d6f1e7d502d1e84b37fe0ed4fb69b1e051e76ee
Aviral Malik <iamaviralmalik@gmail.com>
2025-09-22 14:17:01 +0530

    fixed package vers mismatch

--- frontend/src/queryClient.js
+++ frontend/src/queryClient.js
@@ -5,7 +5,7 @@ export const queryClient = new QueryClient({
     queries: {
       retry: 1,
       refetchOnWindowFocus: false,
-      staleTime: 1000 * 60 * 2, // 2 minutes
+      staleTime: 1000 * 60 * 2,
     },
     mutations: {
       retry: false,
